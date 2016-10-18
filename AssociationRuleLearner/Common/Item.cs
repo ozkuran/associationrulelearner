a89bebb7e948d8db8477d187aff421ed0c48ee7b
Mahmut Ali Özkuran <ozkuran@gmail.com>
2016-10-18 16:11:09 +0300

    Item class created

---
+++ AssociationRuleLearner/Common/Item.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Common
+{
+    public class Item
+    {
+        public Item()
+        {
+            Name = "Test Item";
+            Guid = Guid.NewGuid();
+        }
+
+        public Item(string itemName)
+        {
+            Name = itemName;
+            Guid = Guid.NewGuid();
+        }
+        public string Name { get; set; }
+        public Guid Guid { get; set; }
+    }
+}
