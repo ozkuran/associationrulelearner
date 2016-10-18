e9229793e20f23b97e730b9a803d98c09362eb65
Mahmut Ali Özkuran <ozkuran@gmail.com>
2016-10-18 16:08:52 +0300

    Test project created

---
+++ AssociationRuleLearner/Test/ItemUnitTests.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Common;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+
+namespace Test
+{
+    [TestClass]
+    public class ItemUnitTests
+    {
+        [TestMethod]
+        public void Item_Creation_Default_Constructor_Name()
+        {
+            var item = new Item();
+            const string expected = "Test Item";
+            var actual = item.Name;
+
+            // Assert
+            Assert.AreEqual(expected,actual,"Default constructor not set Item Name correctly");
+        }
+
+        [TestMethod]
+        public void Item_Creation_Default_Constructor_Guid()
+        {
+            var item = new Item();
+
+            var isValid = (item.Guid != Guid.Empty);
+
+            // Assert
+            Assert.AreEqual(true, isValid, "Default constructor not set Item Guid correctly");
+        }
+        
+    }
+}
