fd843a231273ba07e795a3e1af4d58b0adda9bd9
Mahmut Ali Özkuran <ozkuran@gmail.com>
2016-10-20 09:13:20 +0300

    #3 is completed with creation of Rule class. Unit tests for Rule class also created.

---
+++ AssociationRuleLearner/Common/Rule.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security;
+using System.Security.Policy;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Common
+{
+    public class Rule
+    {
+        public Rule()
+        {
+            
+        }
+
+        public Rule(List<Item> items, double support, double confidence)
+        {
+            Items = items;
+            Support = support;
+            Confidence = confidence;
+        }
+
+        public List<Item> Items { get; set; }
+        public double Support { get; set; }
+        public double Confidence { get; set; }
+
+    }
+}
---
+++ AssociationRuleLearner/Test/UnitTestRule.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using Common;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Test
+{
+    [TestClass]
+    public class UnitTestRule
+    {
+        [TestMethod]
+        public void Did_Rule_Created_With_Default_Constructor()
+        {
+            Rule rule = new Rule();
+
+            Assert.IsNotNull(rule, "rule != null");
+        }
+
+        [TestMethod]
+        public void Did_Rule_Created_With_Value_Constructor()
+        {
+            Item item1 = new Item("Test Item 1");
+            Item item2 = new Item("Test Item 2");
+            List<Item> itemList = new List<Item>();
+            itemList.Add(item1);
+            itemList.Add(item2);
+            Rule rule = new Rule(itemList,0.5,0.5);
+
+            Assert.IsNotNull(rule, "rule != null");
+        }
+    }
+}
