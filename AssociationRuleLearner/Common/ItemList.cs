c1db6646ddfe9e2391bd9777b80c9500f6dd0b1f
Mahmut Ali Özkuran <ozkuran@gmail.com>
2016-10-20 13:07:31 +0300

    As ItemList class is created issue #4 is now closed. Unit test of ItemList, UnitTestItemList also created.

---
+++ AssociationRuleLearner/Common/ItemList.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Common
+{
+    public class ItemList
+    {
+        public ItemList()
+        {
+            Items = new HashSet<Item>();
+        }
+
+        public HashSet<Item> Items { get; set; }
+    }
+
+    
+}
