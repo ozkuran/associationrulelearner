cd407fc83605f21fde6bb9823fbf43dfc9be55d7
Mahmut Ali Özkuran <ozkuran@gmail.com>
2016-11-10 09:44:27 +0300

    Improvements on Issue #25/ Added ItemListTransactions class.

---
+++ AssociationRuleLearner/Common/ItemListTransactions.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Common
+{
+    public class ItemListTransactions
+    {
+        public ItemListTransactions()
+        {
+            ItemList = new ItemList();
+            TransactionList = new HashSet<int>();
+        }
+
+        public ItemList ItemList;
+        public HashSet<int> TransactionList;
+    }
+}
