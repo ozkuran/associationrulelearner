8c1c0bd624fdc7712299cbb0b2b3123c7075b176
Mahmut Ali Özkuran <ozkuran@gmail.com>
2016-11-15 17:36:06 +0300

    Improvements on Issue #25.

---
+++ AssociationRuleLearner/Common/ItemTransactions.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Common
+{
+    public class ItemTransactions
+    {
+        public ItemTransactions()
+        {
+            Item = new Item();
+            TransactionList = new HashSet<int>();
+        }
+
+        public ItemTransactions(Item item)
+        {
+            Item = item;
+            TransactionList = new HashSet<int>();
+        }
+
+        public void GetTransactionsFromDatabase(TransactionDatabase transactionDatabase)
+        {
+            foreach (var transactionDatabaseTransaction in transactionDatabase.Transactions)
+            {
+                if (transactionDatabaseTransaction.Items.Contains(Item))
+                {
+                    TransactionList.Add(transactionDatabaseTransaction.Id);
+                }
+            }  
+        }
+
+        public Item Item;
+        public HashSet<int> TransactionList;
+
+
+    }
+}
